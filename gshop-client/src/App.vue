7a1b0790b72cb91d746ba3d4c5d252c1d4f63456
hwkgithub <2965498304@qq.com>
2020-04-21 10:30:54 +0800

    atiProject

---
+++ gshop-client/src/App.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Description: 
+ * @Autor: HWK
+ * @Date: 2020-04-21 09:47:44
+ * @LastEditors: HWK
+ * @LastEditTime: 2020-04-21 10:23:10
+ -->
+<template>
+  <div id="app">
+    <router-view />
+    <FooterGuide v-show="$route.meta.showFooter" />
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+  
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.app {
+  width: 100%;
+  height: 100%;
+  background: #f5f5f5;
+}
+</style>
