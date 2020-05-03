f2cb973f332e7b6ee115d56cc1f6e187588921c3
hwkgithub <2965498304@qq.com>
2020-05-03 17:51:28 +0800

    外卖项目

--- gshop-client_end/src/main.js
+++ gshop-client_end/src/main.js
@@ -1,8 +1,15 @@
 /*
-入口JS
+ * @Description: 入口JS
+ * @Autor: HWK
+ * @Date: 2020-04-23 21:15:00
+ * @LastEditors: HWK
+ * @LastEditTime: 2020-05-03 17:46:07
  */
 import Vue from 'vue'
-import {Button} from 'mint-ui'
+import {
+  Button
+} from 'mint-ui'
+//图片懒加载
 import VueLazyload from 'vue-lazyload'
 import App from './App.vue'
 import router from './router'
@@ -13,7 +20,7 @@ import loading from './common/imgs/loading.gif'
 import './fiters' // 加载过滤器
 
 // 注册全局组件标签
-Vue.component(Button.name, Button)  // <mt-button>
+Vue.component(Button.name, Button) // <mt-button>
 Vue.use(VueLazyload, { // 内部自定义一个指令lazy
   loading
 })
